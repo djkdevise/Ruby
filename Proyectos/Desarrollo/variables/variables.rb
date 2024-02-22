fe2076e8b798a04245a3e4fb8f3bdeec22c2edae
Jamith Garcia <djkdevise@gmail.com>
2024-02-21 23:16:27 -0500

    Inicio formacion Ruby

---
+++ Proyectos/Desarrollo/variables/variables.rb
@@ -0,0 +1,11 @@
+nombre_del_desarrollador = "Jamith"
+
+puts nombre_del_desarrollador
+
+nombre_del_desarrollador = "Pedro"
+
+puts nombre_del_desarrollador
+
+nombre_del_desarrollador = 12545
+
+puts nombre_del_desarrollador
